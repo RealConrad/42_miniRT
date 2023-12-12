7b3bf7624cf7909e3f3a78c46bf4a1329129f65b
eweiberl <eweiberl@student.42heilbronn.de>
2023-12-12 15:55:06 +0100

    opening window

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include "mini_rt.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	while (1)
-		printf("UwU\n");
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+
+	(void)argc;
+	(void)argv;
+	mlx = mlx_init(WIDTH, HEIGHT, "miniRT", true);
+	if (mlx == NULL)
+		return (1);
+	img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	mlx_loop(mlx);
 	return (0);
 }
