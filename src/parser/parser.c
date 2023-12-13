750ad0a1f816d33b1b88689a0ad384185df0812a
eweiberl <eweiberl@student.42heilbronn.de>
2023-12-13 16:33:29 +0100

    imlemented parser_exit

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -26,4 +26,18 @@ void	parser_exit(int error_code, void **free_me)
 		while (free_me[++i] != NULL)
 			free(free_me[i]);
 	}
+	if (error_code == NORMAL)
+		exit(0);
+	ft_fprintf(2, "Error\n");
+	if (error_code == MALLOC_FAIL)
+		ft_fprintf(2, "Malloc fail in parser\n");
+	else if (error_code == OPEN_FAIL)
+		ft_fprintf(2, "Open fail in parser\n");
+	else if (error_code == INPUT_FILE)
+		ft_fprintf(2, "Input file isn't a .rt file\n");
+	else if (error_code == DUPLICATED)
+		ft_fprintf(2, "A, L or C duplicated\n");
+	else if (error_code == MISSING)
+		ft_fprintf(2, " A, L or C is missing\n");
+	exit(1);
 }
