b8306983744e561f32f5d6b2ecd5723dad6f9a3f
eweiberl <eweiberl@student.42heilbronn.de>
2023-12-13 17:05:10 +0100

    t_objects works correctly

--- includes/parser.h
+++ includes/parser.h
@@ -10,75 +10,75 @@
 
 typedef struct s_colour
 {
-	uint8_t		r;
-	uint8_t		g;
-	uint8_t		b;
+	uint8_t			r;
+	uint8_t			g;
+	uint8_t			b;
 }	t_colour;
 
 typedef struct s_vector
 {
-	double		x;
-	double		y;
-	double		z;
+	double			x;
+	double			y;
+	double			z;
 }	t_vector;
 
 typedef struct s_amb_light
 {
-	double		light_ratio;
-	t_colour	colour_in_range;
+	double			light_ratio;
+	t_colour		colour_in_range;
 }	t_amb_light;
 
 typedef struct s_camera
 {
-	t_vector	cords;
-	t_vector	or_vect;
-	uint8_t		fov;
+	t_vector		cords;
+	t_vector		or_vect;
+	uint8_t			fov;
 }	t_camera;
 
 typedef struct s_light
 {
-	t_vector	cords;
-	double		light_ratio;
-	t_colour	colour;
+	t_vector		cords;
+	double			light_ratio;
+	t_colour		colour;
 }	t_light;
 
 typedef struct s_sphere
 {
-	t_vector	cords;
-	double		diameter;
-	t_colour	colour;
+	t_vector		cords;
+	double			diameter;
+	t_colour		colour;
 }	t_sphere;
 
 typedef struct s_plane
 {
-	t_vector	cords;
-	t_vector	threed_vec;
-	t_colour	colour;
+	t_vector		cords;
+	t_vector		threed_vec;
+	t_colour		colour;
 }	t_plane;
 
 typedef struct s_cylinder
 {
-	t_vector	cords;
-	t_vector	axis;
-	double		diameter;
-	double		height;
-	t_colour	colour;
+	t_vector		cords;
+	t_vector		axis;
+	double			diameter;
+	double			height;
+	t_colour		colour;
 }	t_cylinder;
 
 typedef struct s_object
 {
-	t_sphere	*sphere;
-	t_plane		*plane;
-	t_cylinder	*cylinder;
-	t_object	*next;
+	t_sphere		*sphere;
+	t_plane			*plane;
+	t_cylinder		*cylinder;
+	struct s_object	*next;
 }	t_object;
 
 typedef struct s_scene
 {
-	t_amb_light	amb_light;
-	t_camera	camera;
-	t_light		light;
-	t_object	*objects;
+	t_amb_light		amb_light;
+	t_camera		camera;
+	t_light			light;
+	t_object		*objects;
 }	t_scene;
 
 /* -------------------------------------------------------------------------- */
