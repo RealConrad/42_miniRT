ce3fc1a2d4a9a778aeb98a20f0c14e3760184d51
cwenz <wenzconrad@gmail.com>
2023-12-19 20:27:15 +0100

    updated parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,66 +1,66 @@
-
 #include "mini_rt.h"
 
-static void	init_scene(t_scene *scene);
+static void	analize_line(t_scene *scene, char **tokens, int fd);
+static void	init_scene_default(t_scene *scene);
 
-/**
- * @brief takes, checks and converts input
- * @param argc 
- * @param argv 
- * @return the scene struct
- */
-t_scene	parser(int argc, char *argv[])
+t_scene	parser(int argc, char **argv)
 {
 	t_scene	scene;
 	int		fd;
 	char	*line;
+	char	**tokens;
 
 	if (argc != 2)
 		parser_exit(INPUT_MISSING, NULL);
 	if (ft_strncmp(argv[1] + (ft_strlen(argv[1]) - 3), ".rt", 4) != 0)
 		parser_exit(INPUT_FILE, NULL);
 	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-		parser_exit(OPEN_FAIL, NULL);
-	init_scene(&scene);
-	line = get_next_line(fd);
-	if (line == NULL)
-		parser_exit(MISSING, NULL);
-	while (line != NULL)
+	init_scene_default(&scene);
+	while (true)
 	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
 		if (line[ft_strlen(line) - 1] == '\n')
 			line[ft_strlen(line) - 1] = '\0';
-		analize_line(line, &scene, fd);
+		tokens = split_line(line);
 		free(line);
-		line = get_next_line(fd);
+		if (!tokens)
+			parser_exit(MALLOC_FAIL, NULL);
+		analize_line(&scene, tokens, fd);
 	}
 	close(fd);
-	if (scene.amb_light.light_ratio == -1 || scene.camera.fov == -1
-		|| scene.light.light_ratio == -1)
-	{
-		free_objects(&(scene.objects));
-		parser_exit(MISSING, NULL);
-	}
 	return (scene);
 }
 
-/**
- * @brief Initialises the scene to check for double/missing input
- * @param scene the scene to initialise
- */
-static void	init_scene(t_scene *scene)
+static void	analize_line(t_scene *scene, char **tokens, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (tokens[i])
+		i++;
+	if (i == 5 && ft_strncmp(tokens[0], "A", 2) == 0)
+		init_amb(scene, tokens, fd);
+	else if (i == 8 && ft_strncmp(tokens[0], "C", 2) == 0)
+		init_cam(scene, tokens, fd);
+	else if (i == 8 && ft_strncmp(tokens[0], "L", 2) == 0)
+		init_light(scene, tokens, fd);
+	// else if (ft_strncmp(tokens[0], "pl", 3 == 0)
+	// 	&& ft_strncmp(tokens[0], "sp", 3 == 0)
+	// 	&& ft_strncmp(tokens[0], "cy", 3 == 0))
+	// 	init_object(scene, tokens, fd);
+	// else
+	// 	exit_analize(scene, tokens, fd);
+}
+
+static void	init_scene_default(t_scene *scene)
 {
 	scene->amb_light.light_ratio = -1;
 	scene->camera.fov = -1;
 	scene->light.light_ratio = -1;
 }
 
-/**
- * @brief Cleanly exits the parser
- * @param error_code the code for the exit message
- * @param free_me NULLABLE, for freeing allocated memory 
- * takes an unallocated array
- */
 void	parser_exit(int error_code, void **free_me)
 {
 	int	i;
