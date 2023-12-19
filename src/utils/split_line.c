c17b78fd6c0a189ab3421a1131f0d091a67f3012
cwenz <wenzconrad@gmail.com>
2023-12-19 20:27:08 +0100

    added new split funciton

---
+++ src/utils/split_line.c
@@ -0,0 +1,41 @@
+#include "mini_rt.h"
+
+char	**split_line(char *line)
+{
+	char	**tokens;
+	char	*temp;
+	int		i;
+
+	i = 0;
+	temp = ft_strdup(line);
+	if (!temp)
+		return (NULL);
+	while (temp[i])
+	{
+		if (temp[i] == ',')
+			temp[i] = ' ';
+		else if (temp[i] == '\t')
+			temp[i] = ' ';
+		i++;
+	}
+	tokens = ft_split(temp, ' ');
+	free(temp);
+	if (!tokens)
+		return (NULL);
+	return (tokens);	
+}
+
+void	free_2d_array(char **tokens)
+{
+	int	i;
+
+	i = 0;
+	if (!tokens)
+		return ;
+	while (tokens[i])
+	{
+		free(tokens[i]);
+		i++;
+	}
+	free(tokens);
+}
