6d964d21b99491481dee713c2a57550315edd45d
cwenz <wenzconrad@gmail.com>
2023-12-20 14:05:47 +0100

    removed old code

--- includes/parser.h
+++ includes/parser.h
@@ -97,20 +97,4 @@ void		init_amb(t_scene *scene, char **tokens, int fd);
 void		init_cam(t_scene *scene, char **tokens, int fd);
 void		init_light(t_scene *scene, char **tokens, int fd);
 
-// OLD:
-// t_scene		parser(int argc, char *argv[]);
-// void		parser_exit(int error_code, void **free_me);
-// void		free_objects(t_object **objects);
-// void		analize_line(char *line, t_scene *scene, int fd);
-// bool		atof_check(char *str);
-// bool		atoi_check(char *str);
-// void		free_split(char **split);
-// t_colour	get_colour_input(char **split, int colour_index);
-// bool		legal_colour(t_colour colour);
-// t_vector	get_vector_input(char **split, int vec_index);
-// bool		legal_vector(t_vector vector, double min, double max);
-// bool		legal_vector_input(char **split, int vec_index);
-// void		exit_analize(char *line, t_scene *scene, int fd, char **split);
-// void		add_object_to_linked_list(t_scene *scene, t_object *object);
-
 #endif /*PARSER_H*/
