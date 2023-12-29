778b31e8507eef0b825cac4f62c8ecfed1f8d91a
cwenz <wenzconrad@gmail.com>
2023-12-29 11:53:28 +0100

    removed unused variable

--- src/render/vector_operations.c
+++ src/render/vector_operations.c
@@ -2,16 +2,10 @@
 
 t_vector	vec_subtract(t_vector v1, t_vector v2)
 {
-	t_vector	result;
-
-	result = (t_vector){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
-	return (result);
+	return ((t_vector){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z});
 }
 
 t_vector	vec_add(t_vector v1, t_vector v2)
 {
-	t_vector	result;
-
-	result = (t_vector){v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
-	return (result);
+	return ((t_vector){v1.x + v2.x, v1.y + v2.y, v1.z + v2.z});
 }
