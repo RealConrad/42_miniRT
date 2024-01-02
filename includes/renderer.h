6000c8ded3398b372e2b92c0088c23fc689b267d
cwenz <wenzconrad@gmail.com>
2024-01-02 17:09:57 +0100

    added function to renderer header file

--- includes/renderer.h
+++ includes/renderer.h
@@ -10,6 +10,7 @@ void		render_plane(t_scene *scene, t_plane *plane);
 void		render_sphere(t_scene *scene, t_sphere *sphere);
 uint32_t	get_rgb(t_colour colour);
 t_colour	get_ray_colour(t_ray ray, t_object *objects);
+t_colour	get_object_colour(t_object *object);
 
 bool	check_object_intersection(t_object *object, t_ray ray);
 
