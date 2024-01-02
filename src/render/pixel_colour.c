e0cd126c1206e664634ad4af5dbd0b88722da306
cwenz <wenzconrad@gmail.com>
2024-01-02 17:10:45 +0100

    fixed background being inverted

--- src/render/pixel_colour.c
+++ src/render/pixel_colour.c
@@ -1,7 +1,6 @@
 #include "mini_rt.h"
 
 static t_colour	blend_colour(t_colour c1, t_colour c2, double t);
-static t_colour	get_object_colour(t_object *object);
 
 t_colour	get_ray_colour(t_ray ray, t_object *objects)
 {
@@ -10,7 +9,6 @@ t_colour	get_ray_colour(t_ray ray, t_object *objects)
 	t_object	*temp;
 
 	temp = objects;
-	(void)get_object_colour;
 	while (temp)
 	{
 		if (check_object_intersection(temp, ray))
@@ -18,7 +16,7 @@ t_colour	get_ray_colour(t_ray ray, t_object *objects)
 		temp = temp->next;
 	}
 	normalized_ray_direction = normalize_vector(ray.direction);
-	ratio = 0.5  * (normalized_ray_direction.y + 1.0);
+	ratio = 0.5  * (-normalized_ray_direction.y + 1.0);
 	return (blend_colour((t_colour){128, 178, 255}, (t_colour){255, 255, 255}, ratio));
 }
 
