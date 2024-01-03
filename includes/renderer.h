3fb3541749eea9eb523bca0f9e1dac525808a9f9
cwenz <wenzconrad@gmail.com>
2024-01-03 17:17:30 +0100

    updated header file and makefile

--- includes/renderer.h
+++ includes/renderer.h
@@ -10,10 +10,9 @@ void		render_plane(t_scene *scene, t_plane *plane);
 void		render_sphere(t_scene *scene, t_sphere *sphere);
 uint32_t	get_rgb(t_colour colour);
 t_colour	get_ray_colour(t_ray ray, t_object *objects);
-t_colour	get_object_colour(t_object *object);
 
-bool	check_object_intersection(t_object *object, t_ray ray);
-
+double		hit_object(t_object *object, t_ray ray);
+t_vector	ray_at(t_ray ray, double t);
 
 t_vector	vec_add(t_vector v1, t_vector v2);
 t_vector	vec_subtract(t_vector v1, t_vector v2);
