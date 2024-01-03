d57ca6b847e8584a0794191ccfd5cce725665042
cwenz <wenzconrad@gmail.com>
2024-01-03 17:17:12 +0100

    improved ray calculations and made it more readable

--- src/render/intersection.c
+++ src/render/intersection.c
@@ -1,32 +1,31 @@
 #include "mini_rt.h"
 
-static bool		check_sphere_intersection(t_sphere *sphere, t_ray ray);
-static double	dot_product(t_vector v1, t_vector v2);
+static double		hit_sphere(t_sphere *sphere, t_ray ray);
+static double		dot_product(t_vector v1, t_vector v2);
 
-bool	check_object_intersection(t_object *object, t_ray ray)
+double	hit_object(t_object *object, t_ray ray)
 {
-	bool	result;
-
-	result = false;
 	if (object->sphere)
-		result = check_sphere_intersection(object->sphere, ray);
-	return (result);
+		return (hit_sphere(object->sphere, ray));
+	return (-5.0);
 }
 
-static bool	check_sphere_intersection(t_sphere *sphere, t_ray ray)
+static double	hit_sphere(t_sphere *sphere, t_ray ray)
 {
 	t_vector	oc;
 	double		a;
 	double		b;
 	double		c;
+	double		discriminant;
 
 	oc = vec_subtract(ray.origin, sphere->cords);
 	a = dot_product(ray.direction, ray.direction);
 	b = 2.0 * dot_product(oc, ray.direction);
-	c = dot_product(oc, oc) - sphere->diameter * sphere->diameter / 4;
-	if ((b * b - 4 * a * c) < 0)
-		return (false);
-	return (true);
+	c = dot_product(oc, oc) -( sphere->diameter / 2) * (sphere->diameter / 2);
+	discriminant = b * b - 4 * a * c;
+	if (discriminant < 0)
+		return (-1.0);
+	return ((-b - sqrt(discriminant)) / (2.0 * a));
 }
 
 /**
