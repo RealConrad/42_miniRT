d8ecfe7dac7a357f9027d8fb7d626eb8207316a5
cwenz <wenzconrad@gmail.com>
2024-01-05 13:31:12 +0100

    added to_vec function changes

--- src/render/ray_calculations.c
+++ src/render/ray_calculations.c
@@ -11,6 +11,6 @@ t_vector	ray_at(t_ray ray, double t)
 {
 	t_vector	result;
 
-	result = vec_add(ray.origin, vec_multiply(ray.direction, (t_vector){t, t, t}));
+	result = vec_add(ray.origin, vec_multiply(ray.direction, to_vec(t)));
 	return (result);
 }
