c51d4542c6de89271123c04db421c89b230f336a
eweiberl <eweiberl@student.42heilbronn.de>
2024-01-16 16:31:44 +0100

    Moved sources to a different file

---
+++ src/sources.mk
@@ -0,0 +1,23 @@
+
+# ---------------------------------------------------------------------------- #
+#                                 Source Files                                 #
+# ---------------------------------------------------------------------------- #
+
+OBJ_DIR			:= ./objs
+VPATH			:= ./src/ ./src/parser/ ./src/utils/ ./src/debugging/ ./src/input/
+VPATH			+= ./src/free/ ./src/render/ ./src/vec_utils ./src/lighting
+
+SRC				:= main.c
+PARSER_SRC		:= parser.c init_scene.c parser_utils.c init_objects.c input_check.c
+UTIL_SRC		:= progress_bar.c split_line.c colour_utils.c ft_dabs.c rand.c
+FREE_SRC		:= free_objects.c
+INPUT_SRC		:= input.c
+RENDER_SRC		:= render_scene.c ray_hit.c intersection.c ray_calculations.c \
+					mlx.c anti_aliasing.c cylinder.c
+DEBUGGING_SRC	:= t_printing.c
+VEC_UTILS_SRC	:= vector_operations.c vector_operations2.c
+LIGHTING_SRC	:= amb_light.c lighting.c
+
+SRCS			:= $(SRC) $(PARSER_SRC) $(UTIL_SRC) $(DEBUGGING_SRC) $(INPUT_SRC)
+SRCS			+= $(FREE_SRC) $(RENDER_SRC) $(VEC_UTILS_SRC) $(LIGHTING_SRC)
+OBJS			:= $(addprefix $(OBJ_DIR)/, $(SRCS:%.c=%.o))
