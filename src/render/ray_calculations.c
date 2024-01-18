3842cef1947b4eca8904ca573bb888e5daee26ee
cwenz <wenzconrad@gmail.com>
2024-01-18 15:14:46 +0100

    removed unused file

--- src/render/ray_calculations.c
+++ src/render/ray_calculations.c
@@ -15,3 +15,35 @@ t_vector	ray_at(t_ray ray, double t)
 	result = vec_add(ray.origin, vec_multiply(ray.direction, to_vec(t)));
 	return (result);
 }
+
+/**
+ * @brief Gets the ray information
+ * @param ray 
+ * @param objects 
+ * @return true if the ray hits anything, otherwise false
+ */
+bool	get_ray_intersection(t_ray *ray, t_object *objects)
+{
+	t_object	*temp;
+	double		distance;
+	t_object	*closest;
+	double		old_hit;
+
+	closest = NULL;
+	temp = objects;
+	old_hit = 0;
+	while (temp)
+	{
+		distance = hit_object(temp, ray);
+		if ((distance < old_hit || old_hit == 0) && distance > 0.0)
+		{
+			closest = temp;
+			old_hit = distance;
+		}
+		temp = temp->next;
+	}
+	if (closest == NULL)
+		return (false);
+	hit_object(closest, ray);
+	return (true);
+}
