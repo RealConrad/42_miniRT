e8cd0e975488a7a894a2b294b086d1fc1295b823
cwenz <wenzconrad@gmail.com>
2024-01-24 15:44:47 +0100

    changed variable name for amb struct

--- includes/lighting.h
+++ includes/lighting.h
@@ -8,7 +8,7 @@
 /*                                  Functions                                 */
 /* -------------------------------------------------------------------------- */
 
-t_colour	get_ambient_light(t_colour obj_colour, t_amb_light amb);
+t_colour	get_ambient_light(t_amb_light amb, t_colour object_colour);
 void		lighting(t_scene *scene, t_ray *ray);
 void		lighting2(t_scene *scene, t_ray *ray);
 
