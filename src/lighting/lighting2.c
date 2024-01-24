3c31288184f03e195160e01505473d133724f11c
eweiberl <eweiberl@student.42heilbronn.de>
2024-01-24 16:56:50 +0100

    tried adding light

--- src/lighting/lighting2.c
+++ src/lighting/lighting2.c
@@ -3,6 +3,40 @@
 static t_colour colour_multiply(t_colour c1, t_colour c2);
 static t_colour colour_scalar_multiply(t_colour c, double scalar);
 
+static t_ray	get_light_ray(t_light light, t_vector hit_point)
+{
+	t_ray	light_ray;
+
+	light_ray.origin = hit_point;
+	light_ray.direction = vec_subtract(light.cords, hit_point);
+	light_ray.direction = normalize_vector(light_ray.direction);
+	light_ray.hit_point = light.cords;
+	// if (hit_point.y > light.cords.y)
+	// 	light_ray.direction.y *= -1;
+	return (light_ray);
+}
+
+
+static bool	light_hit(t_ray ray, t_object *objects)
+{
+	t_object	*temp;
+	double		old_hit;
+	double		distance;
+
+	temp = objects;
+	old_hit = 0;
+	while (temp != NULL)
+	{
+		distance = hit_object(temp, &ray);
+		if ((distance < old_hit || old_hit == 0) && distance > 0.0)
+			old_hit = distance;
+		temp = temp->next;
+	}
+	if (old_hit == 0)
+		return (true);
+	return (false);
+}
+
 void	lighting2(t_scene *scene, t_ray *ray)
 {
 	t_colour	amb;
@@ -13,6 +47,8 @@ void	lighting2(t_scene *scene, t_ray *ray)
 	t_colour	specular;
 	double		shiny;
 
+	t_ray	light_ray = get_light_ray(scene->light, ray->hit_point);
+
 	shiny = 25;
 	amb = get_ambient_light( ray->ray_colour, scene->amb_light);
 
@@ -21,7 +57,7 @@ void	lighting2(t_scene *scene, t_ray *ray)
 	{
 		light_dir = normalize_vector(vec_subtract(scene->light.cords, ray->hit_point));
 		dot = dot_product(ray->surface_norm, light_dir);
-		if (dot > 0)
+		if (dot > 0 && light_hit(light_ray, scene->objects))
 		{
 			diffuse = colour_multiply(scene->light.colour, ray->ray_colour);
 			diffuse = colour_scalar_multiply(diffuse, dot * scene->light.light_ratio);
@@ -49,7 +85,7 @@ void	lighting2(t_scene *scene, t_ray *ray)
 			// ray->ray_colour.b = clamp(amb.b + diffuse.b, 0, 255);
 		}
 		else
-			ray->ray_colour = amb;
+			ray->ray_colour = (t_colour){0, 0, 0};
 	}
 	else
 	{
