be39ceac89e8098f81a8e16e9ac5686cd6eb3d2c
eweiberl <eweiberl@student.42heilbronn.de>
2024-01-29 17:26:38 +0100

    norm and doxy comment

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,6 +1,11 @@
 
 #include "mini_rt.h"
 
+/**
+ * @brief Gets a random double
+ * @return Returns a double between -0.5 and 0.5
+ * @note arc4random is used since AA isn't part of the mandatory
+ */
 double	random_double(void)
 {
 	return (((arc4random() / (double)UINT32_MAX) - 0.5));
