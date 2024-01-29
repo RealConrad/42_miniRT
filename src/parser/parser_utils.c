a49062aeb9c0f2a92b9fa78a5385203284b6f0f8
cwenz <wenzconrad@gmail.com>
2024-01-29 13:31:50 +0100

    moved function in parsesr

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -32,7 +32,7 @@ void	exit_analyse(t_scene *scene, char **tokens, int fd, t_object *object)
  * @param vector The vector struct to assign data to.
  * @param tokens An array of strings containing the data.
  * @param start_index The start index used for the `tokens`.
- * The 3 positions (x, y, z) should be stored in tokens consecutively.
+ * @note The 3 positions (x, y, z) should be stored in tokens consecutively.
  */
 void	assign_vector(t_vector *vector, char **tokens, int start_index)
 {
@@ -47,11 +47,29 @@ void	assign_vector(t_vector *vector, char **tokens, int start_index)
  * @param colour The colour struct to assign data to.
  * @param tokens An array of strings containing the data.
  * @param start_index The start index used for the `tokens`.
- * The 3 colours (red, green, blue) should be stored in tokens consecutively.
+ * @note The 3 colours (red, green, blue) should be stored in tokens consecutively.
  */
 void	assign_colour(t_colour *colour, char **tokens, int start_index)
 {
 	colour->r = ft_atoi(tokens[start_index]);
 	colour->g = ft_atoi(tokens[start_index + 1]);
 	colour->b = ft_atoi(tokens[start_index + 2]);
+}
+
+/**
+ * @brief Checks if the vector is legal
+ * @param vector the vector to check
+ * @param min the minimal value
+ * @param max the max value
+ * @return true if in range, false
+ */
+bool	legal_vector(t_vector vector, double min, double max)
+{
+	if (vector.x < min || vector.x > max)
+		return (false);
+	if (vector.y < min || vector.y > max)
+		return (false);
+	if (vector.z < min || vector.z > max)
+		return (false);
+	return (true);
 }
